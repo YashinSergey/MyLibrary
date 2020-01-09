449e22c1f57ca270665f22e6198b8bd989cf0369
mr.Blue <yashinsergey125@gmail.com>
2020-01-09 23:17:14 +0300

    created book repository

--- app/src/main/java/com/iashinsergei/mylibrary/data/BookRepos.java
+++ app/src/main/java/com/iashinsergei/mylibrary/data/BookRepos.java
@@ -1,4 +1,35 @@
-package com.iashinsergei.mylibrary.data.entity;
+package com.iashinsergei.mylibrary.data;
+
+import com.iashinsergei.mylibrary.data.entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookRepos {
+
+    private static BookRepos instance;
+
+    private List<Book> bookList;
+
+    private BookRepos() {
+        bookList = new ArrayList<>();
+        bookList.add(new Book("war novel","Erich Maria Remarque","","Three Comrades",1936));
+        bookList.add(new Book("drama","William Shakespeare","","Hamlet",1602));
+        bookList.add(new Book("historical fiction","Margaret Mitchell","","Gone with the Wind",1936));
+    }
+
+    private static BookRepos getInstance() {
+        if (instance == null) {
+            instance = new BookRepos();
+        }
+        return instance;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public int getBookListSize() {
+        return bookList.size();
+    }
 }
