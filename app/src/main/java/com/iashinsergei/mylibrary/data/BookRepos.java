1da985dc1ccb4ae1bff2aec05ddbea92cf9af33c
mr.Blue <yashinsergey125@gmail.com>
2020-01-10 15:24:20 +0300

    made getInstance synchronized

--- app/src/main/java/com/iashinsergei/mylibrary/data/BookRepos.java
+++ app/src/main/java/com/iashinsergei/mylibrary/data/BookRepos.java
@@ -18,7 +18,7 @@ public class BookRepos {
         bookList.add(new Book("historical fiction","Margaret Mitchell","","Gone with the Wind","", 1, 1936));
     }
 
-    public static BookRepos getInstance() {
+    public static synchronized BookRepos getInstance() {
         if (instance == null) {
             instance = new BookRepos();
         }
