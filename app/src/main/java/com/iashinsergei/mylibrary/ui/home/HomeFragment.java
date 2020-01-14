b818deb9bc9ef8ecde86a7bf3154924243b640ab
mr.Blue <yashinsergey125@gmail.com>
2020-01-14 16:18:40 +0300

    added recyclerveiw to HomeFragment

--- app/src/main/java/com/iashinsergei/mylibrary/ui/home/HomeFragment.java
+++ app/src/main/java/com/iashinsergei/mylibrary/ui/home/HomeFragment.java
@@ -11,25 +11,27 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.iashinsergei.mylibrary.R;
+import com.iashinsergei.mylibrary.model.BookRepos;
+import com.iashinsergei.mylibrary.ui.adapters.RvHomeAdapter;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        initRecyclerView(root);
         return root;
+    }
+
+    private void initRecyclerView(View view){
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        RecyclerView recyclerView = view.findViewById(R.id.rv_home);
+        recyclerView.setLayoutManager(layoutManager);
+        RvHomeAdapter adapter = new RvHomeAdapter(BookRepos.getInstance().getBookList());
+        recyclerView.setAdapter(adapter);
     }
 }
