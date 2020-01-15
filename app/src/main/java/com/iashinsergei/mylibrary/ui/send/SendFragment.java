087db18a5764bce970e17d654c9c183e49f83d79
mr.Blue <yashinsergey125@gmail.com>
2020-01-15 15:53:15 +0300

    removed viewModels from fragments

--- app/src/main/java/com/iashinsergei/mylibrary/ui/send/SendFragment.java
+++ app/src/main/java/com/iashinsergei/mylibrary/ui/send/SendFragment.java
@@ -16,20 +16,8 @@ import com.iashinsergei.mylibrary.R;
 
 public class SendFragment extends Fragment {
 
-    private SendViewModel sendViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        sendViewModel =
-                ViewModelProviders.of(this).get(SendViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
-        sendViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
--- app/src/main/java/com/iashinsergei/mylibrary/ui/share/ShareFragment.java
+++ app/src/main/java/com/iashinsergei/mylibrary/ui/share/ShareFragment.java
@@ -16,20 +16,8 @@ import com.iashinsergei.mylibrary.R;
 
 public class ShareFragment extends Fragment {
 
-    private ShareViewModel shareViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        shareViewModel =
-                ViewModelProviders.of(this).get(ShareViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
